8f8731a80a0c0c86481430bb2beca3e4da1b0ae7
ostki <ostkir@gmail.com>
2021-04-25 22:10:48 +0300

    9.2. Checkstyle [#523972]

--- src/ru/job4j/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/ru/job4j/condition/TrgArea.java
@@ -1,8 +1,9 @@
 package ru.job4j.ru.job4j.condition;
 
 public class TrgArea {
+    @SuppressWarnings("checkstyle:ParenPad")
     public static double area(double a, double b, double c) {
-        return Math.sqrt(((a + b + c)/2) * ( ((a + b + c)/2) - a) * (((a + b + c)/2) - b) * (((a + b + c)/2) - c));
+        return Math.sqrt(((a + b + c) / 2) * (((a + b + c) / 2) - a) * (((a + b + c) / 2) - b) * (((a + b + c) / 2) - c));
     }
 
     public static void main(String[] args) {
