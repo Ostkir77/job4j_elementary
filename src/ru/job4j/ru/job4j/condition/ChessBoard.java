dbb88faa010937972998300156b850c01073541a
ostki <ostkir@gmail.com>
2021-04-30 19:56:54 +0300

    4.4.3.6. Math.abs [#523915]

--- src/ru/job4j/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/ru/job4j/condition/ChessBoard.java
@@ -2,6 +2,13 @@ package ru.job4j.ru.job4j.condition;
 
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
-        return 0;
+        int rsl = 0;
+        if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
+            rsl = Math.abs(x2 - x1);
+        }
+        if (x1 * x2 * y1 * y2 < 0) {
+            rsl = 0;
+        }
+        return rsl;
     }
 }
