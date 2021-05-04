ccc85ca4fa524fbd319bb8eea99796cf23b2a8df
ostki <ostkir@gmail.com>
2021-05-04 10:25:44 +0300

    5.7. Ипотека [#524016]

--- src/ru/job4j/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/ru/job4j/loop/Mortgage.java
@@ -4,7 +4,7 @@ public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
         double debt = amount;
-        while (debt >= 0) {
+        while (debt > 0) {
             debt = debt + amount * percent / 100 - salary;
             year++;
         }
