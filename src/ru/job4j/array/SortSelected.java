135db7d1e84c6acbf9d3a3e9ed49dbf532e4caf2
ostki <ostkir@gmail.com>
2021-05-10 13:15:13 +0300

    6.6.3. Двухмерный массив. If. [#523946]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -2,12 +2,10 @@ package ru.job4j.array;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
-        for (int index = 0; index <= data.length - 1; index++) {
+        for (int index = 0; index < data.length; index++) {
             int min = MinDiapason.findMin(data, index, data.length - 1);
-            int minIndex = FindLoop.indexOfDiapason(data, min, 0, data.length - 1);
-            int temp = data[minIndex];
-            data[minIndex] = data[index];
-            data[index] = temp;
+            int minIndex = FindLoop.indexOfDiapason(data, min, index, data.length - 1);
+            SwitchArray.swap(data, index, minIndex);
         }
         return data;
     }
